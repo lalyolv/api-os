9397897124cbdf1a4ee559a509804ec5a5280dc0
Larissa Oliveira <larissaoliveira.dev@gmail.com>
2025-03-15 19:56:54 -0400

    chore: inicializacao usuario teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { AutenticacaoModule } from './autenticacao/autenticacao.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // variáveis do .env
+    AutenticacaoModule
+  ]
 })
 export class AppModule {}
