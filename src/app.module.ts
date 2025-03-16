11f7e14e358c00978b52038e9b71904231947913
Larissa Oliveira <larissaoliveira.dev@gmail.com>
2025-03-15 21:33:10 -0400

    chore: module de servico adicionado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AutenticacaoModule } from './autenticacao/autenticacao.module'
+import { OrdensServicoModule } from './ordens_servico/ordens_servico.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // variáveis do .env
-    AutenticacaoModule
+    AutenticacaoModule,
+    OrdensServicoModule
   ]
 })
 export class AppModule {}
