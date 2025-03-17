ed01b521f46ad95e063adebb7ca47593d18c05eb
Larissa Oliveira <larissaoliveira.dev@gmail.com>
2025-03-17 09:47:20 -0400

    chore: add url do frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  // Habilita CORS para permitir conexões do frontend
+  app.enableCors({
+    origin: 'http://localhost:3001', // URL do frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
